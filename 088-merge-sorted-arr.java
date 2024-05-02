db6f3adedec04f4180ee7acefac857c0c813c408
MoKi <Mokiwins@gmail.com>
2024-05-01 22:04:33 -0400

    tired

--- 088-merge-sorted-arr.java
+++ 088-merge-sorted-arr.java
@@ -1,6 +1,3 @@
-
-
-
 /* 
 Problem:
 
@@ -10,7 +7,10 @@ and two integers m and n, representing the number of elements in nums1 and nums2
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n,
+where the first m elements denote the elements that should be merged,
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
  
 
@@ -20,12 +20,14 @@ Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
 Example 2:
 
 Input: nums1 = [1], m = 1, nums2 = [], n = 0
 Output: [1]
 Explanation: The arrays we are merging are [1] and [].
 The result of the merge is [1].
+
 Example 3:
 
 Input: nums1 = [0], m = 0, nums2 = [1], n = 1
@@ -44,5 +46,11 @@ nums2.length == n
 -109 <= nums1[i], nums2[j] <= 109
  
 
-Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+come up with an algorithm that runs in O(m + n) time?
 */
+
+class Solution{
+    public void merge(){
+        
+    }
+}
